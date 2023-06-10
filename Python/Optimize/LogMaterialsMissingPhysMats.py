0a34742204178f0125f7df03c6b0b75632147a39
wail <wail-gaming@protonmail.com>
2023-06-10 11:42:55 -0400

    Adding Optimization Python Scripts

---
+++ Python/Optimize/LogMaterialsMissingPhysMats.py
@@ -0,0 +1,78 @@
+import unreal
+import sys
+import os
+
+notepadFilePath = os.path.dirname(__file__) + "/Output.txt"
+
+if (len(sys.argv) > 1):
+    workingPath = sys.argv[1]
+else:
+    workingPath = "/Game/" # Using the root directory
+
+EditAssetLib = unreal.EditorAssetLibrary()
+SystemsLib = unreal.SystemLibrary
+allAssets = EditAssetLib.list_assets(workingPath, True, False)
+selectedAssetsPath = workingPath
+LogStringsArray = []
+numOfOptimisations = 0
+
+
+with unreal.ScopedSlowTask(len(allAssets), selectedAssetsPath) as ST:
+    ST.make_dialog(True)
+
+    for asset in allAssets:
+        _assetData = EditAssetLib.find_asset_data(asset)
+        _assetName = _assetData.get_asset().get_name()
+        _assetPathName = _assetData.get_asset().get_path_name()
+        _assetClassName = _assetData.get_asset().get_class().get_name()
+
+        if _assetClassName == "Material" or _assetClassName == "MaterialInstanceConstant":
+            asset_obj = EditAssetLib.load_asset(asset)
+            StringToCheck = _assetData.get_asset().get_editor_property("phys_material")  # asset_obj.get_editor_property("phys_material")
+            # unreal.log("Used Phys Mat = %s" % StringToCheck)
+
+            if not SystemsLib.is_valid(StringToCheck):
+                LogStringsArray.append("        %s ------------> At Path: %s \n" % (_assetName, _assetPathName))
+                # unreal.log("Asset Name: %s Path: %s \n" % (_assetName, _assetPathName))
+                numOfOptimisations += 1
+
+        if ST.should_cancel():
+            break
+        ST.enter_progress_frame(1, asset)
+
+
+# Write results into a log file
+# //////////////////////////////
+TitleOfOptimisation = "Log Materials With Missing Physical Materials"
+DescOfOptimisation = "Searches the entire project for materials that don't have a phys mats plugged in"
+SummaryMessageIntro = "-- Materials Without Phys Mats Plugged In --"
+
+if unreal.Paths.file_exists(notepadFilePath):  # Check if txt file already exists
+    os.remove(notepadFilePath)  # if does remove it
+
+# Create new txt file and run intro text
+file = open(notepadFilePath, "a+")  # we should only do this if have a count?
+file.write("==================================================================================================== \n")
+file.write("    SCRIPT NAME: %s \n" % TitleOfOptimisation)
+file.write("    DESCRIPTION: %s \n" % DescOfOptimisation)
+file.write("==================================================================================================== \n \n")
+
+
+if numOfOptimisations <= 0:
+    file.write(" -- NONE FOUND -- \n \n")
+else:
+    for i in range(len(LogStringsArray)):
+        file.write(LogStringsArray[i])
+
+
+# Run summary text
+file.write("\n")
+file.write("======================================================================================================= \n")
+file.write("    SUMMARY: \n")
+file.write("        %s \n" % SummaryMessageIntro)
+file.write("              Found: %s \n \n" % numOfOptimisations)
+file.write("======================================================================================================= \n")
+file.write("        Logged to %s \n" % notepadFilePath)
+file.write("======================================================================================================= \n")
+file.close()
+os.startfile(notepadFilePath)  # Trigger the notepad file to open
---
+++ Python/Optimize/StaticMeshNumOfMaterials.py
@@ -0,0 +1,80 @@
+
+import unreal
+import sys
+import os
+
+notepadFilePath = os.path.dirname(__file__) + "/Output.txt"
+
+if (len(sys.argv) > 1):
+    workingPath = sys.argv[1]
+else:
+    workingPath = "/Game/" # Using the root directory
+    
+NumOfMatsToCheckFor = int(float(sys.argv[2]))
+
+EditAssetLib = unreal.EditorAssetLibrary()
+StatMeshLib = unreal.EditorStaticMeshLibrary()
+allAssets = EditAssetLib.list_assets(workingPath, True, False)
+selectedAssetsPath = workingPath
+LogStringsArray = []
+numOfOptimisations = 0
+
+
+with unreal.ScopedSlowTask(len(allAssets), selectedAssetsPath) as ST:
+    ST.make_dialog(True)
+
+    for asset in allAssets:
+        _assetData = EditAssetLib.find_asset_data(asset)
+        _assetName = _assetData.get_asset().get_name()
+        _assetPathName = _assetData.get_asset().get_path_name()
+        _assetClassName = _assetData.get_asset().get_class().get_name()
+
+        if _assetClassName == "StaticMesh":
+            _staticMeshAsset = unreal.StaticMesh.cast(_assetData.get_asset())
+            _howManyMaterials = StatMeshLib.get_number_materials(_staticMeshAsset)
+            # unreal.log(StatMeshLib.get_number_materials(_staticMeshAsset))
+
+            if _howManyMaterials >= NumOfMatsToCheckFor:
+                LogStringsArray.append("        [%s] - %s ------------> At Path: %s \n" % (_howManyMaterials, _assetName, _assetPathName))
+                unreal.log("Asset Name: [%s] %s Path: %s \n" % (_howManyMaterials, _assetName, _assetPathName))
+                numOfOptimisations += 1
+
+        if ST.should_cancel():
+            break
+        ST.enter_progress_frame(1, asset)
+
+# Write results into a log file
+# //////////////////////////////
+TitleOfOptimisation = "LOG Static Meshes Assets With X Num Of Materials"
+DescOfOptimisation = "Searches the entire project for static mesh assets that have a material count above the value that you have set"
+SummaryMessageIntro = "-- Static Mesh Assets With Material Count Numbering >= %s --" % NumOfMatsToCheckFor
+
+if unreal.Paths.file_exists(notepadFilePath):  # Check if txt file already exists
+    os.remove(notepadFilePath)  # if does remove it
+
+# Create new txt file and run intro text
+file = open(notepadFilePath, "a+")  # we should only do this if have a count?
+file.write("==================================================================================================== \n")
+file.write("    SCRIPT NAME: %s \n" % TitleOfOptimisation)
+file.write("    DESCRIPTION: %s \n" % DescOfOptimisation)
+file.write("==================================================================================================== \n \n")
+
+
+if numOfOptimisations <= 0:
+    file.write(" -- NONE FOUND -- \n \n")
+else:
+    for i in range(len(LogStringsArray)):
+        file.write(LogStringsArray[i])
+
+
+# Run summary text
+file.write("\n")
+file.write("======================================================================================================= \n")
+file.write("    SUMMARY: \n")
+file.write("        %s \n" % SummaryMessageIntro)
+file.write("              Found: %s \n \n" % numOfOptimisations)
+file.write("======================================================================================================= \n")
+file.write("        Logged to %s \n" % notepadFilePath)
+file.write("======================================================================================================= \n")
+file.close()
+os.startfile(notepadFilePath)  # Trigger the notepad file to open
